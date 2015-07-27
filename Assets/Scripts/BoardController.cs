c6cf67f9d72df48e17ac64e55fe455e31befcd07
jcamberos <tucamberos@gmail.com>
2015-07-26 21:21:36 -0700

    Videos # 1 - 20 Had to restart game due to problems with visual studio

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardController : MonoBehaviour {
+
+    public int columns;
+    public int rows;
+
+    public GameObject[] floors;
+
+    private Transform gameBoard;
+	
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+	
+	}
+    private void SetupGameBoard()
+    {
+        gameBoard = new GameObject("Game Board").transform;
+
+        for(int x = 0; x < columns; x++)
+        {
+            for(int y = 0; y < rows; y++)
+            {
+               // GameObject floorTile =(GameObject)Instantiate(floor, new Vector3(x, y, 0f), Quaternion.identity);
+                //floorTile.transform.SetParent(gameBoard);
+            }
+
+        }
+
+    }
+    public void SetupLever()
+    {
+        SetupGameBoard();
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController Instance;
+
+    private BoardController boardController;
+
+
+	void Awake () {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        boardController = GetComponent<BoardController>();	
+	}
+	
+	void Start()
+    {
+        boardController.SetupLever();
+    }
+
+	void Update () {
+	
+	}
+}

d7d0e86e19f70a73203f2a9a0457757b357ddd22
jcamberos <tucamberos@gmail.com>
2015-07-26 23:03:44 -0700

    Outer Walls Created outer walls using if statements and selecting random as an option.

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -7,6 +7,7 @@ public class BoardController : MonoBehaviour {
     public int rows;
 
     public GameObject[] floors;
+    public GameObject[] outerWalls;
 
     private Transform gameBoard;
 	
@@ -24,11 +25,20 @@ public class BoardController : MonoBehaviour {
 
         for(int x = 0; x < columns; x++)
         {
-            for(int y = 0; y < rows; y++)
+            for (int y = 0; y < rows; y++)
             {
-                GameObject selectedTile = floors[Random.Range(0, floors.Length)];
+                GameObject selectedTile;
+                if (x == 0 || y == 0 || x == columns - 1 || y == rows - 1)
+                {
+                    selectedTile = outerWalls[Random.Range(0, outerWalls.Length)];
+                }
+                else
+                {
+                    selectedTile = floors[Random.Range(0, floors.Length)];
+                }
+                
                 GameObject floorTile =(GameObject)Instantiate(selectedTile, new Vector3(x, y, 0f), Quaternion.identity);
-                //floorTile.transform.SetParent(gameBoard);
+                floorTile.transform.SetParent(gameBoard);
             }
 
         }

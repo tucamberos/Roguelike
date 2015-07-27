5440d00b71408cda624d24cd1a56c327ec59cb84
jcamberos <tucamberos@gmail.com>
2015-07-27 14:13:56 -0700

    generated setup turn based, player movement, and setup for smooth movement. Player was moving too fast

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -3,12 +3,33 @@ using System.Collections;
 
 public class MovingObject : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rigidBody;
+
+
+	protected virtual void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+
 	}
 	
-	// Update is called once per frame
+    protected bool CanObjectMove(int xDirection, int yDirection)
+    {
+        Vector2 startPosition = rigidBody.position;
+        Vector2 endPosition = startPosition + new Vector2(xDirection, yDirection);
+
+
+        StartCoroutine(SmoothMovementRoutine(endPosition));
+
+        return true;
+
+    }
+	
+    protected IEnumerator SmoothMovementRoutine(Vector2 endPosition)
+    {
+        //Implement a swet of instructions to smooth out movement
+    }
+
 	void Update () {
 	
 	}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MovingObject {
+
+	
+	void Update () {
+        int xAxis = 0;
+        int yAxis = 0;
+
+        xAxis = (int)Input.GetAxisRaw("Horizontal");
+        yAxis = (int)Input.GetAxisRaw("Vertical");
+
+        CanObjectMove(xAxis, yAxis);
+
+	}
+}

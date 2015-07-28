b071680313a7c6870a144424defeffeb89fc1c1f
jcamberos <tucamberos@gmail.com>
2015-07-27 21:50:57 -0700

    box colliders Added box colliders and implemented collisions with walls and other items in the map

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -5,26 +5,34 @@ public class MovingObject : MonoBehaviour {
 
     private BoxCollider2D boxCollider;
     private Rigidbody2D rigidBody;
+    private LayerMask CollisionLayer;
 
 
 	protected virtual void Start () {
         boxCollider = GetComponent<BoxCollider2D>();
         rigidBody = GetComponent<Rigidbody2D>();
-
+        CollisionLayer = LayerMask.GetMask("Collision Layer");
 	}
-	
+
     protected bool CanObjectMove(int xDirection, int yDirection)
     {
         Vector2 startPosition = rigidBody.position;
         Vector2 endPosition = startPosition + new Vector2(xDirection, yDirection);
 
+        boxCollider.enabled = false;
+        RaycastHit2D hit = Physics2D.Linecast(startPosition, endPosition, CollisionLayer);
+        boxCollider.enabled = true;
 
-        StartCoroutine(SmoothMovementRoutine(endPosition));
+        if (hit.transform == null)
+        {
 
-        return true;
+            StartCoroutine(SmoothMovementRoutine(endPosition));
+            return true;
+        }
 
+        return false;
     }
-	
+        
     protected IEnumerator SmoothMovementRoutine(Vector2 endPosition)
     {
         float remainingDistanceToEndPosition;
@@ -32,7 +40,7 @@ public class MovingObject : MonoBehaviour {
         do
         {
             remainingDistanceToEndPosition = (rigidBody.position = endPosition).sqrMagnitude;
-            Vector2 updatedPosition = Vector2.MoveTowards(rigidBody.position, endPosition, 9f * Time.deltaTime);
+            Vector2 updatedPosition = Vector2.MoveTowards(rigidBody.position, endPosition, 20f * Time.deltaTime);
             rigidBody.MovePosition(updatedPosition);
             yield return null;
 

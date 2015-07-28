e3a6cf85ba6b1bac07f7d3051458b6bfbf2cef15
jcamberos <tucamberos@gmail.com>
2015-07-28 14:46:54 -0700

    Enemy Access
    Created code to allow enemies to attack or walk towards player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class Enemy : MovingObject {
 
-	
+    private Transform player;
+
+
 	protected override void Start () {
         GameController.Instance.AddEnemyToList(this);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         base.Start();
 	
 	}
